42c5603f678e82305e1eedf7eebbb5eb10ec5742
TheMaximuzz <maks.natorin@gmail.com>
2024-10-06 17:45:43 +0500

    .

---
+++ SportX_Bot/src/main/java/org/example/bot/UserProfile.java
@@ -0,0 +1,8 @@
+package org.example.bot;
+
+public class UserProfile {
+    public String nickname;
+    public int age;
+    public int height;
+    public int weight;
+}
---
+++ SportX_Bot/src/main/java/org/example/bot/UserState.java
@@ -0,0 +1,9 @@
+package org.example.bot;
+
+public enum UserState {
+    ENTER_NICKNAME,
+    ENTER_AGE,
+    ENTER_HEIGHT,
+    ENTER_WEIGHT,
+    COMPLETED
+}
